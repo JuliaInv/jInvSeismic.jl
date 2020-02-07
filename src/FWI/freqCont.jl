29ef48e60d56464f819a236e5fe3c0d034234475
sagibu <sagibuch93@gmail.com>
2020-02-07 11:11:12 +0200

    clean code of freqCont

--- src/FWI/freqCont.jl
+++ src/FWI/freqCont.jl
@@ -182,6 +182,8 @@ for freqIdx = startFrom:(length(contDiv)-1)
 
 		Dp,pForp = getData(vec(mc), pForpCurrent);
 
+
+		#Calculate HinPs
 		for freqs = 1:nworkers():numOfCurrentProblems
 			pForCurrent = pForp[freqs]
 			pMisCurrent[freqs].pFor = pForCurrent
@@ -271,6 +273,8 @@ for freqIdx = startFrom:(length(contDiv)-1)
 			return sum + alpha * R + stepReg * R
 		end
 
+
+		#Merge data for each frequency
 		numOfFreqs = length(1:nwork:numOfCurrentProblems);
 		mergedSourcesArr = Vector{Array}(undef,numOfFreqs)
 		mergedDobsArr = Vector{Array}(undef, numOfFreqs)
@@ -301,22 +305,19 @@ for freqIdx = startFrom:(length(contDiv)-1)
 			Rc[i] = (avgWds[i]^2) .* (mergedDobsArr[i] - HPinvsReduced[i]' * mergedSourcesArr[i])
 		end
 
-		# Rsum = sum(Rc)
-
 		rhs = zeros(ComplexF64, size(Z1))
 		for i = 1:numOfFreqs
 			rhs += MultOpT(HPinvsReduced[i], Rc[i], Z2)
 		end
 		rhs += stepReg * Z1
 
-		# save("ext.jld", "hps", HPinvsReduced, "dobs", mergedDobsArr, "wd", mergedWdArr,
-		# "src", mergedSourcesArr)
 		Z1 = KrylovMethods.blockBiCGSTB(x-> MultAll(avgWds, HPinvsReduced, x, Z2, alpha1, stepReg), rhs,x=Z1,maxIter=10, out=2)[1];
-		# throw("abs")
 		println("misfit at Z1:: ", misfitCalc2())
 
 	end
 
+
+	# Update the pMis with new sources
 	newSrc = Z1*Z2
 	for i=1:numOfCurrentProblems
 		pMisCurrent[i].pFor.Sources = pMisCurrent[i].pFor.OriginalSources + newSrc[:,currentSrcInd[i]]
@@ -329,12 +330,14 @@ for freqIdx = startFrom:(length(contDiv)-1)
 		pMisCurrent[freqs].pFor = pForCurrent
 	end
 
+	# Update all sources
 	for i= 1:length(pMis)
 		temp = take!(pMis[i])
 		temp.pFor.Sources = temp.pFor.OriginalSources + newSrc[:, sourcesSubInd[i]]
 		put!(pMis[i], temp)
 	end
 
+	#update sources for current problems, maybe not needed?
 	for i=1:numOfCurrentProblems
 		temp = take!(pMisTemp[i])
 		temp.pFor.Sources = temp.pFor.OriginalSources + newSrc[:, currentSrcInd[i]]
@@ -380,170 +383,3 @@ for freqIdx = startFrom:(length(contDiv)-1)
 end
 return mc,Dc,flag,HIS;
 end
-
-#
-# function freqContExtendedSources(mc, pInv::InverseParam, pMis::Array{RemoteChannel},
-# 	nfreq::Int64, windowSize::Int64, Iact,
-# 	mback::Union{Vector,AbstractFloat,AbstractModel, Array{Float64,1}},
-# 	dumpFun::Function, resultsFilename::String, startFrom::Int64 = 1)
-# Dc = 0;
-# flag = -1;
-# HIS = [];
-#
-# pFor = fetch(pMis[1]).pFor
-# mSizeMat = pFor.Mesh.n .+ 1;
-# m = mSizeMat[1];
-# n = mSizeMat[2];
-# mSizeVec = mSizeMat[1] * mSizeMat[2];
-# # Z = copy(pFor.originalSources);
-# # sizeQ = size(Z);
-# nrec = size(pFor.Receivers, 2);
-# sizeH = size(pFor.Ainv[1]);
-# pFor = nothing
-# nsrc = size(pFor.Sources, 2);
-# alpha = 2e-3;
-# p = 10;
-#
-# for freqIdx = startFrom:nfreq
-# 	Z1 = rand(ComplexF64,(m*n, p)) .+ 0.01;
-# 	Z2 = rand(ComplexF64, (p, nsrc)) .+ 0.01;
-# 	println("start freqCont Zs iteration from: ", freqIdx)
-# 	tstart = time_ns();
-# 	reqIdx1 = freqIdx;
-# 	if freqIdx > 1
-# 		reqIdx1 = max(1,freqIdx-windowSize+1);
-# 	end
-# 	reqIdx2 = freqIdx;
-# 	currentProblems = reqIdx1:reqIdx2;
-#
-#
-# 	println("\n======= New Continuation Stage: selecting continuation batches: ",reqIdx1," to ",reqIdx2,"=======\n");
-# 	runningProcs = map(x->x.where, pMis[currentProblems]);
-#
-#
-# 	for j=1:5
-#
-# 		pMisCurrent = map(fetch, pMis[currentProblems]);
-# 		pForpCurrent =  map(x->x.pFor, pMisCurrent);
-# 		Dp,pForp = getData(vec(mc), pForpCurrent);
-#
-# 		pForCurrent = map(x->fetch(x), pForp);
-# 		numOfCurrentProblems = size(currentProblems, 1);
-# 		map((pm,pf) -> pm.pFor = pf , pMisCurrent, pForCurrent);
-# 		HinvPs = Vector{Array}(undef, numOfCurrentProblems);
-# 		pMisTemp = Array{RemoteChannel}(undef, length(currentProblems));
-# 		t1 = time_ns();
-# 		for freqs = 1:numOfCurrentProblems
-# 			U,Ainv = solveLinearSystem(H,U,Ainv,0)
-# 			HinvPs[freqs] = (pForCurrent[freqs].Ainv)' \ Matrix(pForCurrent[freqs].Receivers);
-# 			println("HINVP done");
-# 		end
-# 		e1 = time_ns();
-# 		println("runtime of HINVPs");
-# 		println((e1 - t1)/1.0e9);
-#
-# 		if resultsFilename == ""
-# 				filename = "";
-# 			else
-# 				Temp = splitext(resultsFilename);
-# 				filename = string(Temp[1],"_FC",freqIdx,"_",j,"_GN",Temp[2]);
-# 		end
-#
-# 		function dumpGN(mc,Dc,iter,pInv,PF)
-# 			dumpFun(mc,Dc,iter,pInv,PF,filename);
-# 		end
-#
-# 		t2 = time_ns();
-# 		Dc,F,dF,d2F,pMisNone,times,indDebit = computeMisfit(mc, pMisCurrent);
-# 		e2 = time_ns();
-# 		println("Misfit B4 mzs at GN ", j, "frequncy idx: ", freqIdx, " Is: ", F);
-# 		println("runtime of compute misfit");
-# 		println((e2 - t2)/1.0e9);
-#
-# 		t3 = time_ns();
-#
-# 		pMisArr = map(pm -> fetch(pm), pMisCurrent);
-# 		Ap = Vector{Matrix}(undef, numOfCurrentProblems);
-# 		diags = Vector{SparseMatrixCSC}(undef, numOfCurrentProblems);
-# 		B = Vector{Vector}(undef, numOfCurrentProblems);
-#
-# 		toVec(mat) = reshape(mat, mSizeVec);
-# 		toMat(vec) = reshape(vec, tuple(mSizeMat...));
-#
-# 		Wd = map(pm -> pm.Wd[:,:,1], pMisCurrent)
-# 		function misfitCalc2()
-# 			sum = 0;
-# 			for i = 1:numOfCurrentProblems
-# 				sum += (mean(Wd[i])^2) .* norm(HinvPs[i]' * (pMisCurrent[i].pFor.Sources + Z1 * Z2) - pMisCurrent[i].dobs[:,:,1])^2;
-# 			end
-#
-# 			sum	+= alpha * norm(Z1)^2 + alpha * norm(Z2)^2;
-# 			return sum;
-# 		end
-#
-# 		Z2 = calculateZ2(misfitCalc2, p, nsrc, numOfCurrentProblems, Wd, HinvPs,
-# 			pMisCurrent, Z1, alpha);
-#
-# 		println("misfit at Z2:: ", misfitCalc2())
-#
-# 		function multOP(R,HinvP)
-# 			return HinvP' * R * Z2;
-# 		end
-#
-# 		function multOPT(R,HinvP)
-# 			return HinvP * R * Z2';
-# 		end
-#
-# 		function multAll(x)
-# 			sum = zeros(ComplexF64, (mSizeVec, p));
-#
-# 			for i = 1:numOfCurrentProblems
-# 				sum += (mean(Wd[i])^2) .* multOPT(multOP(x, HinvPs[i]), HinvPs[i]);
-# 			end
-#
-# 			sum += alpha * x;
-# 			return sum;
-# 		end
-#
-# 		rhs = zeros(ComplexF64, (mSizeVec, p));
-# 		for i = 1:numOfCurrentProblems
-# 			pm = pMisCurrent[i]
-# 			rhs += (mean(Wd[i])^2).*multOPT(-HinvPs[i]' * pm.pFor.Sources + pm.dobs[:,:,1], HinvPs[i]);
-# 		end
-#
-# 		Z1 = KrylovMethods.blockBiCGSTB(x-> real(multAll(x)), real(rhs))[1];
-#
-# 		for i = 1:numOfCurrentProblems
-# 			pMisCurrent[i].pFor.Sources += Z1 * Z2;
-# 		end
-#
-# 		println("misfit at Z1:: ", misfitCalc2())
-#
-# 		Dc,F,dF,d2F,pMisNone,times,indDebit = computeMisfit(mc, pMisCurrent);
-#
-# 		println("Misfit after GN ", j, "frequncy idx: ", freqIdx, " Is: ", F);
-# 		pMispCurrent = Array{RemoteChannel}(undef, numOfCurrentProblems);
-# 		for i=1:numOfCurrentProblems
-# 			pMispCurrent[i] = initRemoteChannel(x->x, runningProcs[i], pMisCurrent[i]);
-# 		end
-# 		pInv.mref = mc[:];
-# 		t4 = time_ns();
-# 		mc,Dc,flag,His = projGNCG(mc,pInv,pMispCurrent,dumpResults = dumpGN);
-# 		e4 = time_ns();
-# 		println((t4 - e4)/1.0e9);
-# 		Dc,F,dF,d2F,pMisNone,times,indDebit = computeMisfit(mc,pMisCurrent);
-#
-# 		println("Misfit after GN ", j, "frequncy idx: ", freqIdx, " Is: ", F);
-#
-# 		pMis[currentProblems] = pMispCurrent;
-# 		clear!(pMispCurrent);
-# 	end
-#
-# 	tend = time_ns();
-#     println("Runtime of freqCont iteration: ");
-#     println((tend - tstart)/1.0e9);
-# 	global inx = inx + 1;
-#
-# end
-# return mc,Dc,flag,HIS;
-# end
